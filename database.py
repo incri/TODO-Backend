5fd9a90d5e761fdb2c1b63b46bb18b9c18fe71b6
incri <paudelbaibhav0@gmail.com>
2023-07-27 16:09:38 +0545

    ready for deploy

--- database.py
+++ database.py
@@ -1,10 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from config import DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME
+
+import os
+
+# Use the environment variables to construct the database connection URL
+db_host = os.environ.get("DB_HOST")
+db_port = os.environ.get("DB_PORT")
+db_user = os.environ.get("DB_USER")
+db_password = os.environ.get("DB_PASSWORD")
+db_name = os.environ.get("DB_NAME")
 
 SQLALCHEMY_DATABASE_URL = (
-    f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 )
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
