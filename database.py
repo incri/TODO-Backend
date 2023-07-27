04becfd7ed755d4b67d0fd6ce0c9e5802004e002
incri <paudelbaibhav0@gmail.com>
2023-07-27 16:05:17 +0545

    sucessfully connected to render hosted database

--- database.py
+++ database.py
@@ -3,20 +3,9 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from config import DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME
 
-import os
-
-# Get the database connection details from environment variables
-db_host = os.environ.get("DB_HOST")
-db_port = os.environ.get("DB_PORT")
-db_user = os.environ.get("DB_USER")
-db_password = os.environ.get("DB_PASSWORD")
-db_name = os.environ.get("DB_NAME")
-
-# Use the database connection details to create the connection string
 SQLALCHEMY_DATABASE_URL = (
-    f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+    f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 )
-
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
