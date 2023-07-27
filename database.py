8d6eeb86be36e0ffb6c82d42241411008e8eb85a
incri <paudelbaibhav0@gmail.com>
2023-07-27 15:21:10 +0545

    todo app completed

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgres://todos_databse_user:ZEJE0K2hmPMo0VcXUk7OAZLfk3QQjAhw@dpg-cj12sj5ph6enmk775mjg-a.singapore-postgres.render.com/todos_databse"
+SQLALCHEMY_DATABASE_URL = "postgresql://incri:fastrack@localhost/todos"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
